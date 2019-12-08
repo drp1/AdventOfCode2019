d9f6fa38b087c3820693c170effbb72eb1608614
Dan Peterjohn <drp7.acc@gmail.com>
2019-12-08 07:53:01 -0600

    Finished Day 4 - Part 2

--- Day4/Day4.cpp
+++ Day4/Day4.cpp
@@ -1,9 +1,71 @@
 #include <gtest/gtest.h>
 
+int GetDigitAtIndex(int const num, int const index, int const numDigits)
+{
+	int resultDigit = num;
+	resultDigit /= static_cast<int>(pow(10, numDigits - index - 1)); 
+	resultDigit %= 10;
+	return resultDigit;
+}
+
+bool NumberContainsOnlyTwoAdjacentDigits(int const num, int const numDigits)
+{
+	int i = 0;
+
+	// Left edge of window
+	if (numDigits < 2)
+	{
+		return false;
+	}
+	if (numDigits == 2)
+	{
+		int const leftDigit = GetDigitAtIndex(num, i, numDigits);
+		int const rightDigit = GetDigitAtIndex(num, i + 1, numDigits);
+		return leftDigit == rightDigit;
+	}
+	if (numDigits > 2) {
+		int const leftDigit = GetDigitAtIndex(num, i, numDigits);
+		int const centerDigit = GetDigitAtIndex(num, i + 1, numDigits);
+		int const rightDigit = GetDigitAtIndex(num, i + 2, numDigits);
+
+		if (leftDigit == centerDigit && centerDigit != rightDigit)
+		{
+			return true;
+		}
+	}
+	
+	// Center
+	while (++i <= numDigits - 2 - 1) // Minus 2 because we are checking two elements ahead, and minus 1 because zero-based index
+	{
+		int const leftDigit = GetDigitAtIndex(num, i - 1, numDigits);
+		int const leftCenterDigit = GetDigitAtIndex(num, i, numDigits);
+		int const rightCenterDigit = GetDigitAtIndex(num, i + 1, numDigits);
+		int const rightDigit = GetDigitAtIndex(num, i + 2, numDigits);
+
+		if (leftCenterDigit == rightCenterDigit && (leftDigit != leftCenterDigit && rightDigit != rightCenterDigit))
+		{
+			return true;
+		}
+	}
+
+	// Right edge of window
+	int const leftDigit = GetDigitAtIndex(num, i - 1, numDigits);
+	int const centerDigit = GetDigitAtIndex(num, i, numDigits);
+	int const rightDigit = GetDigitAtIndex(num, i + 1, numDigits);
+
+	if (leftDigit != centerDigit && centerDigit == rightDigit)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+
 bool NumberContainsTwoOrMoreAdjacentDigits(int num, int numDigits)
 {
 	int lastDigit = num % 10; // Isolate least significant digit
-	for (int i = 0; i < numDigits; i++)
+	for (int i = 1; i < numDigits; i++)
 	{
 		num /= 10; // Remove least significant digit
 		int currentDigit = num % 10;
@@ -19,7 +81,7 @@ bool DigitsDoNotDecreaseLeftToRight(int num, int numDigits)
 	// It is actually easier to prove if we find there are no increasing numbers from right to left
 	
 	int lastDigit = num % 10; // Isolate least significant digit
-	for (int i = 0; i < numDigits; i++)
+	for (int i = 1; i < numDigits; i++)
 	{
 		num /= 10; // Remove least significant digit
 		int currentDigit = num % 10;
@@ -31,6 +93,23 @@ bool DigitsDoNotDecreaseLeftToRight(int num, int numDigits)
 	return true;
 }
 
+TEST(Day4TestSuite, Should_Return_Correct_Digit)
+{
+	EXPECT_EQ(GetDigitAtIndex(12345, 0, 6), 0);
+	EXPECT_EQ(GetDigitAtIndex(12345, 1, 6), 1);
+	EXPECT_EQ(GetDigitAtIndex(12345, 2, 6), 2);
+	EXPECT_EQ(GetDigitAtIndex(12345, 3, 6), 3);
+	EXPECT_EQ(GetDigitAtIndex(12345, 4, 6), 4);
+	EXPECT_EQ(GetDigitAtIndex(12345, 5, 6), 5);
+}
+
+
+TEST(Day4TestSuite, Should_Return_Numbers_With_At_Only_Two_Adajcent_Numbers)
+{
+	EXPECT_TRUE(NumberContainsOnlyTwoAdjacentDigits(112233, 6));
+	EXPECT_FALSE(NumberContainsOnlyTwoAdjacentDigits(123444, 6));
+	EXPECT_TRUE(NumberContainsOnlyTwoAdjacentDigits(111122, 6));
+}
 
 TEST(Day4TestSuite, Should_Return_Numbers_With_At_Least_Two_Adajcent_Numbers)
 {
@@ -63,4 +142,23 @@ TEST(Day4TestSuite, Part1)
 	}
 
 	std::cout << "Part 1: " << numberOfValidNumbers;
+}
+
+
+TEST(Day4TestSuite, Part2)
+{
+	int const numberOfDigits = 6;
+	int numberOfValidNumbers = 0;
+	int start = 347312, end = 805915;
+
+	for (int i = start; i <= end; i++)
+	{
+		if (!NumberContainsOnlyTwoAdjacentDigits(i, numberOfDigits))
+			continue;
+		if (!DigitsDoNotDecreaseLeftToRight(i, numberOfDigits))
+			continue;
+		numberOfValidNumbers++;
+	}
+
+	std::cout << "Part 2: " << numberOfValidNumbers;
 }
