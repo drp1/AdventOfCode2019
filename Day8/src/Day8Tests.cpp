9ed0ced751c0dae0c4cf163dfd5e8873df8b7404
Dan Peterjohn <drp7.acc@gmail.com>
2019-12-31 13:28:45 -0600

    Complete Day 8 - Part 2

--- Day8/src/Day8Tests.cpp
+++ Day8/src/Day8Tests.cpp
@@ -20,10 +20,19 @@ private:
 	int const height, width;
 };
 
+enum PixelColor
+{
+	Black = 0,
+	White = 1,
+	Transparent = 2
+};
+
 // Function declarations;
 int FindLayerWithLeastZeros(Image const& image);
 int NumberOfSpecificDigitInLayer(Image const& image, int const layer, int const digit);
 Image* CreateImage(istream& stream, int const height, int const width);
+void PrintDecodedImage(Image const& decodedImage);
+Image* CreateDecodedImage(Image const& encodedImage);
 
 int FindLayerWithLeastZeros(Image const& image)
 {
@@ -89,6 +98,59 @@ Image* CreateImage(istream& stream, int const height, int const width)
 	return image;
 }
 
+Image* CreateDecodedImage(Image const& encodedImage)
+{
+	int const height = encodedImage.GetHeight();
+	int const width = encodedImage.GetWidth();
+	Image* decodedImage = new Image(encodedImage.GetHeight(), encodedImage.GetWidth());
+
+	int** imageLayer = new int* [height];
+	for (int i = 0; i < height; ++i)
+		imageLayer[i] = new int[width];
+	decodedImage->data.push_back(imageLayer);
+
+	// Set all pixels to transparent in decoded image
+	for (int h = 0; h < decodedImage->GetHeight(); ++h)
+	{
+		for(int w = 0; w < decodedImage->GetWidth(); ++w)
+		{
+			decodedImage->data[0][h][w] = Transparent;
+		}
+	}
+
+	for (int layer = 0; layer < encodedImage.GetNumLayers(); ++layer)
+	{
+		for(int h = 0; h < height; ++h)
+		{
+			for (int w = 0; w < width; ++w)
+			{
+				if (decodedImage->data[0][h][w] == Transparent && encodedImage.data[layer][h][w] != Transparent)
+				{
+					decodedImage->data[0][h][w] = encodedImage.data[layer][h][w];
+				}
+			}
+		}
+	}	
+
+	return decodedImage;
+}
+
+void PrintDecodedImage(Image const& decodedImage)
+{
+	int const height = decodedImage.GetHeight();
+	int const width = decodedImage.GetWidth();
+	cout << "P1" << endl;
+	cout << width << " " << height << endl;
+	for (int h = 0; h < height; ++h)
+	{
+		for (int w = 0; w < width; ++w)
+		{
+			cout << decodedImage.data[0][h][w] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
 
 TEST(Day8TestSuite, Test_NumberOfSpecificDigitInLayer)
 {
@@ -151,7 +213,6 @@ TEST(Day8TestSuite, Test_FindLayerWithLeastZeros)
 
 }
 
-
 TEST(Day8TestSuite, ExampleImage)
 {
 	string const imageData = "123456789012";
@@ -188,6 +249,35 @@ TEST(Day8TestSuite, ExampleImage)
 	EXPECT_EQ(imageNumLayers, actualImage->GetNumLayers());
 }
 
+TEST(Day8TestSuite, ExampleDecodedImage)
+{
+	string const imageData = "0222112222120000";
+	stringstream ss(imageData);
+	int const imageWidth = 2;
+	int const imageHeight = 2;
+	int const imageNumLayers = 1;
+	int expectedDecodedImage[imageHeight][imageWidth] = {
+		{0,1},
+		{1,0}
+	};
+
+	auto const actualImage = CreateImage(ss, imageHeight, imageWidth);
+	auto const actualDecodedImage = CreateDecodedImage(*actualImage);
+
+	EXPECT_EQ(imageWidth, actualDecodedImage->GetWidth());
+	EXPECT_EQ(imageHeight, actualDecodedImage->GetHeight());
+	EXPECT_EQ(imageNumLayers, actualDecodedImage->GetNumLayers());
+	
+	for (int height = 0; height < imageHeight; ++height)
+	{
+		for (int width = 0; width < imageWidth; ++width)
+		{
+			EXPECT_EQ(expectedDecodedImage[height][width], actualDecodedImage->data[0][height][width]);
+		}
+	}
+}
+
+
 TEST(Day8TestSuite, Part1)
 {
 	int const width = 25;
@@ -205,4 +295,22 @@ TEST(Day8TestSuite, Part1)
 	int const answer = num1Digits * num2Digits;
 	cout << answer << endl;
 	EXPECT_EQ(answer, 2048);
+}
+
+TEST(Day8TestSuite, Part2)
+{
+	int const width = 25;
+	int const height = 6;
+	string const filePath = "src/input.txt";
+	ifstream fin(filePath);
+	if (!fin.is_open())
+		throw exception("File not found");
+
+	Image* const image = CreateImage(fin, height, width);
+	Image* const decodedImage = CreateDecodedImage(*image);
+
+	// Outputs string in PBM format.
+	PrintDecodedImage(*decodedImage);
+
+	//EXPECT_EQ(answer, HFYAK);
 }
